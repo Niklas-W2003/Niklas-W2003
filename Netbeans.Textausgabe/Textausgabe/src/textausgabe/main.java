29ac3065f6fe3946145f6ecfd5b066c8914e1959
Niklas-W2003 <nik286.wark@gmail.com>
2021-10-04 12:51:09 +0200

    Add files via upload

---
+++ Netbeans.Textausgabe/Textausgabe/src/textausgabe/main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textausgabe;
+
+/**
+ *
+ * @author niklas.warkentin
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int x=0;
+        //do{
+        //System.out.println("Test mainmethode"); //"Test mainmethode wird ausgegeben"
+        //text.gibtext(); //Die methode gibtext aus der klasse text wird aufgerufen
+        //x++;// x + 1
+        //System.out.println(x);// Die varible x wird als zahl ausgegeben
+        //}while(x<5);
+        Hauptfenster hauptf = new Hauptfenster();
+        hauptf.setVisible(true);
+    }
+}
---
+++ Netbeans.Textausgabe/Textausgabe/src/textausgabe/text.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textausgabe;
+
+/**
+ *
+ * @author niklas.warkentin
+ */
+public class text {
+        
+    public static void gibtext(){
+    System.out.println("Text Methode");// gibt den Text "Text Methode"
+    }
+   
+}
